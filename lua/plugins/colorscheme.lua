fab202c38777f91904d201531fbf7612cca30484
ziyulab <ziyedeyuu@163.com>
2025-11-10 00:37:28 +0800

    基本能用的配置

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+      transparent_background = true,
+      float = { transparent = true },
+      auto_integration = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
